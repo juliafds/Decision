b53bd4f7fe3a9490b1168af40029d205cfb5cfea
juliafds <juliafds2001@gmail.com>
2019-11-06 10:23:15 -0300

    update

--- decision.php
+++ decision.php
@@ -202,11 +202,11 @@ END;
 </div>
 
 
-<div id="section6" class="container-fluid" style="padding-top:70px;padding-bottom:70px; background-color: #ffffff;">
+<div id="section6" class="container-fluid" style="padding-top:70px;padding-bottom:70px; background-image: url(imagens/mercado.jpg); background-attachment: fixed; color: white;">
         <div class="container">
             <div class="row">
                 <div class="col-12 text-center my-4">
-                    <h1 class="display-4">Mercado de trabalho</h1>
+                    <h1 class="display-4">Mercado de Trabalho</h1>
                     <br>
                     <p align="justify">
                         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
