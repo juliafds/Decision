4d403933f207829e89bfe9eb0d201c70b6e41707
juliafds <juliafds2001@gmail.com>
2019-12-10 16:21:46 -0300

    update

--- rodape.php
+++ rodape.php
@@ -9,7 +9,7 @@
             <!-- Grid column -->
             <div class="col-md-2 mb-3">
                 <h6 class="text-uppercase font-weight-bold">
-                    <a style="color: #19256c;">Contato</a>
+                    <a style="color: #19256c;">Desenvolvedores</a>
                 </h6>
             </div>
             <!-- Grid column -->
@@ -28,28 +28,32 @@
             <img class="card-img-top" img src="Imagens/cubo.jpeg" style="border-radius: 100%; max-width:200px;
                  max-height:150px; width: auto; height: auto;">
             <b><p>Kezia de Jesus de Souza</p></b>
+            <p>keziajsouz@gmail.com </p>
         </div>
 
         <div class="col-sm" style="text-align: center;">
             <img class="card-img-top" img src="Imagens/ju.jpeg" style="border-radius: 100%; max-width:200px;
                  max-height:150px; width: auto; height: auto;">
             <b><p>Júlia Ferreira da Silva</p></b>
+            <p>juliafds2001@gmail.com</p>
         </div>
 
         <div class="col-sm" style="text-align: center;">
             <img class="card-img-top" img src="Imagens/gabi.jpeg" style="border-radius: 100%; max-width:200px;
              max-height:150px; width: auto; height: auto;">
             <b><p>Gabriela dos Santos Neves</p></b>
+            <p>gabrielasantosneves11@gmail.com</p>
         </div>
 
         <div class="col-sm" style="text-align: center;">
             <img class="card-img-top" img src="Imagens/bia.jpeg" style="border-radius: 100%; max-width:200px;
              max-height:150px; width: auto; height: auto;">
-            <b><p>Bianca Dubberstein de Souza Moura</p></b>
+            <b><p>Bianca Dubberstein de Souza</p></b>
+            <p>biancavirtual10@gmail.com</p>
         </div>
         </div>
     </div>
-
+    <br>
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3" style="color: #19256c;"> © 2019 Copyright:
